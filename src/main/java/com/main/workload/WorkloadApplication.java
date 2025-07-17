fb3e49856da5fb5ab2e949d58666dea23686ba39
avnikittsev <nikittsev.anton@gmail.com>
2025-07-17 03:11:44 +0300

    Fix cors once again (hopefully for the last time)

--- src/main/java/com/main/workload/WorkloadApplication.java
+++ src/main/java/com/main/workload/WorkloadApplication.java
@@ -2,10 +2,6 @@ package com.main.workload;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -13,23 +9,6 @@ public class WorkloadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkloadApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(@NonNull CorsRegistry registry) {
-				registry
-						.addMapping("/api")
-						.allowedOrigins("*")
-						.allowedMethods("*");
-				registry
-						.addMapping("/error")
-						.allowedOrigins("*")
-						.allowedMethods("*");
-			}
-		};
 	}
 
 }
